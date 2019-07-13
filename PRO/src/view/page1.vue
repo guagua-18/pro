7b2448d0f59fe52fd917cb286b4470aa7a748945
likang <lkworks@126.com>
2019-07-13 16:56:20 +0800

    commit my first project

---
+++ PRO/src/view/page1.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <el-row>
+        <el-col :span="8">
+            
+        </el-col>
+        <el-col :span="16"></el-col>
+    </el-row>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
